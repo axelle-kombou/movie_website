5c2ae60bc81197853c3483eca7749297bd11ec9b
axelle-kombou <axelle.sandjong@gmail.com>
2016-06-15 17:50:55 -0400

    Update media.py

--- media.py
+++ media.py
@@ -2,7 +2,8 @@ import webbrowser
 # The class Movie can be think of as a blueprint and it contains data(eg:movie_title...) and method(eg:show_trailer).
 class Movie():
 # This method is used to initialize (define) all of the data associated to our examples or instances (eg:toy_story).
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube): '''initializing function'''
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        """[the initializing function]"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
